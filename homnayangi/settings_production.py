d0224b31d088f9f1e14bc1487209fc7b439da17e
kinoubenkyou <kinoubenkyou@gmail.com>
2024-11-12 17:28:06 +0700

    WIP

--- homnayangi/settings_production.py
+++ homnayangi/settings_production.py
@@ -8,3 +8,13 @@ ALLOWED_HOSTS = environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
 DATABASES["default"]["HOST"] = environ.get("POSTGRES_HOST")
 
 DEBUG = False
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+}
