8651673fdf4f21c89a0ae78805cc6afec3ceb03c
kinoubenkyou <kinoubenkyou@gmail.com>
2024-11-23 20:26:46 +0700

    Release (#3)

---
+++ homnayangi/settings_production.py
@@ -0,0 +1,27 @@
+from os import environ
+
+from homnayangi.settings_base import *
+
+
+ALLOWED_HOSTS = environ["DJANGO_ALLOWED_HOSTS"].split(",")
+
+DATABASES["default"]["HOST"] = environ["POSTGRES_HOST"]
+DATABASES["default"]["PASSWORD"] = environ["POSTGRES_PASSWORD"]
+
+DEBUG = False
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
+
+SECRET_KEY = environ["DJANGO_SECRET_KEY"]
